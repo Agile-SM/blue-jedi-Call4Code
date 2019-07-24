ebfbfc37d3f33ab8598621967602008c13e1121d
Oihane <seveoih@gmail.com>
2019-07-24 13:42:11 +0200

    test

--- server.js
+++ server.js
@@ -4,13 +4,14 @@ const express = require("express"),
   appEnv = require("cfenv").getAppEnv(),
   app = express();
 
-  app.use(cors());
+console.log("POR QUE?")
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Routes
 let router = express.Router();
-
+console.log("LAS RUTAS?")
 router.use("/iot", require("./routes/iot"));
 router.use("/arcgis", require("./routes/arcgis"));
 router.use("/dashboard", require("./routes/dashboard"));
@@ -19,11 +20,10 @@ router.use("/vr", require("./routes/visual-recognition"));
 
 // all routes will be prefixed with /api
 app.use("/api", router);
-
+console.log("PETA AQUI?")
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
-
+console.log("PERO POR QUE?")
 /* app.listen(port, () => { */
 app.listen(appEnv.port, appEnv.bind, () => {
   console.log(`App listening on http://localhost:${appEnv.port}`);
