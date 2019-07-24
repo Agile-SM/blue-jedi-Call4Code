72928ce9acfb3d231cd50bbe07167552bbf15738
Oihane <seveoih@gmail.com>
2019-07-24 13:49:47 +0200

    n

--- server.js
+++ server.js
@@ -4,23 +4,19 @@ const express = require("express"),
   appEnv = require("cfenv").getAppEnv(),
   app = express();
 
-console.log("POR QUE?")
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 // Routes
 let router = express.Router();
-console.log("LAS RUTAS?")
 router.use("/dashboard", require("./routes/dashboard"));
 /* router.use("/cron", require("./routes/cron")); */
 
 // all routes will be prefixed with /api
 app.use("/api", router);
-console.log("PETA AQUI?")
 app.use(express.json());
 
-console.log("PERO POR QUE?")
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 /* app.listen(port, () => { */
 app.listen(appEnv.port, appEnv.bind, () => {
   console.log(`App listening on http://localhost:${appEnv.port}`);
