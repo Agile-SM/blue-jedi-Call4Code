277847ac773dc3791dada5b242c044d7505afdcc
Oihane <seveoih@gmail.com>
2019-07-24 14:12:33 +0200

    test

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express"),
-  bodyParser = require("body-parser"),
   cors = require("cors"),
   appEnv = require("cfenv").getAppEnv(),
   app = express();
@@ -9,12 +8,12 @@ app.use(cors());
 // Routes
 /* let router = express.Router(); */
 app.use("/api/dashboard", require("./routes/dashboard"));
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 /* app.use("/api", router); */
 /* app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+
 /* app.listen(port, () => { */
 app.listen(appEnv.port, appEnv.bind, () => {
   console.log(`App listening on http://localhost:${appEnv.port}`);
